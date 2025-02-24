9fedc0ab95e2c9781d85df0b099ad17ee84ca60e
Jeff Foxx <foxx@adobe.com>
2025-02-24 15:48:58 -0500

    signin

---
+++ blocks/sign-in/sign-in.js
@@ -0,0 +1,57 @@
+import ffetch from '../../scripts/ffetch.js';
+
+const allusers = await ffetch('/admin/users.json').all();
+function findUser(arr, query) {
+  return arr.filter((el) => el.username === query);
+}
+
+function handleLogin() {
+  const form = document.querySelector('#static-signin-form');
+  const username = form.querySelector('input[name=username]').value;
+  const userMatch = findUser(allusers, username);
+  if (userMatch.length > 0) {
+    form.classList.add('submitting');
+    localStorage.setItem('firstName', userMatch[0].firstName);
+    localStorage.setItem('lastName', userMatch[0].lastName);
+
+    setTimeout(() => {
+      window.location = '/';
+    }, '2000');
+  } else {
+    const errorMessage = document.createElement('p');
+    errorMessage.className = 'error-message';
+    errorMessage.innerText = 'User does not match';
+    form.prepend(errorMessage);
+  }
+}
+
+function createSignInForm(block) {
+  const formContainer = document.createElement('div');
+  formContainer.id = 'static-signin-form';
+  formContainer.className = 'panel pure-form pure-form-aligned';
+
+  formContainer.innerHTML = `
+        <div class="pure-control-group">
+            <label for="username">Username</label>
+            <input name="username" type="email" autocomplete="username">
+        </div>
+        <div class="pure-control-group">
+            <label for="password">Password</label>
+            <input name="password" type="password" autocomplete="password">
+        </div>
+        <div class="pure-controls">
+            <p><a href="/" data-se="recover-password" onclick="_showRecoverPassword(event)">Forgot your password?</a></p>
+            <button class="button" >Sign In</button>
+        </div>
+    `;
+
+  block.appendChild(formContainer);
+
+  const submitButton = formContainer.querySelector('button');
+  submitButton.addEventListener('click', handleLogin);
+}
+
+export default function decorate(block) {
+  block.textContent = '';
+  createSignInForm(block);
+}
